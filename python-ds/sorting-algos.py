4c79f60389166007b5da50578850cb7bd0b31b65
Eddy Mogollon <eddymogollon@Eddys-MacBook-Pro.local>
2019-06-16 19:35:10 -0400

    Added insertion sort

--- python-ds/sorting-algos.py
+++ python-ds/sorting-algos.py
@@ -78,3 +78,39 @@ def selectionSort(A):
 myArr2 = [5, 3, 7, 1, 98, 65, 0]
 selectionSort(myArr2)
 print(myArr2)
+
+# Python program for implementation of Insertion Sort 
+  
+
+"""
+mark first element as sorted
+
+for each unsorted element X
+  'extract' the element X
+  for j = lastSortedIndex down to 0
+    if current element j > X
+      move sorted element to the right by 1
+    break loop and insert X here
+"""
+
+# Function to do insertion sort 
+def insertionSort(arr): 
+  
+    # Traverse through 1 to len(arr) 
+    for i in range(1, len(arr)): 
+  
+        key = arr[i] 
+  
+        # Move elements of arr[0..i-1], that are 
+        # greater than key, to one position ahead 
+        # of their current position 
+        j = i-1
+        while j >= 0 and key < arr[j] : 
+                arr[j + 1] = arr[j] 
+                j -= 1
+        arr[j + 1] = key 
+  
+  
+# Driver code to test above 
+arr = [12, 11, 13, 5, 6] 
+insertionSort(arr) 
